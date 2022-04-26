2e32847ada3ef280fde15022ebc37dc7581a776d
Jaime <alu0101329093@ull.edu.es>
2022-04-26 10:33:03 +0100

    Updated exchange info

--- include/vrp/exchange_info.h
+++ include/vrp/exchange_info.h
@@ -11,37 +11,41 @@ class ExchangeInfo {
  public:
   ExchangeInfo(const ClientInfo& first_client, const ClientInfo& second_client,
                std::size_t first_position, std::size_t second_position,
-               std::size_t route);
+               std::size_t first_route, std::size_t second_route);
 
   inline const ClientInfo& GetFirstClient() const { return first_client_; }
-  inline void SetFirstClient(const ClientInfo& first_client) {
-    first_client_ = first_client;
+  inline void SetFirstClient(const ClientInfo& client) {
+    first_client_ = client;
   }
 
   inline const ClientInfo& GetSecondClient() const { return second_client_; }
-  inline void SetSecondClient(const ClientInfo& second_client) {
-    second_client_ = second_client;
+  inline void SetSecondClient(const ClientInfo& client) {
+    second_client_ = client;
   }
 
   inline std::size_t GetFirstPosition() const { return first_position_; }
-  inline void SetFirstPosition(std::size_t first_position) {
-    first_position_ = first_position;
+  inline void SetFirstPosition(std::size_t position) {
+    first_position_ = position;
   }
 
   inline std::size_t GetSecondPosition() const { return second_position_; }
-  inline void SetSecondPosition(std::size_t second_position) {
-    second_position_ = second_position;
+  inline void SetSecondPosition(std::size_t position) {
+    second_position_ = position;
   }
 
-  inline std::size_t GetRoute() const { return route_; }
-  inline void SetRoute(std::size_t route) { route_ = route; }
+  inline std::size_t GetFirstRoute() const { return first_route_; }
+  inline void SetFirstRoute(std::size_t route) { first_route_ = route; }
+
+  inline std::size_t GetSecondRoute() const { return second_route_; }
+  inline void SetSecondRoute(std::size_t route) { second_route_ = route; }
 
  private:
   ClientInfo first_client_;
   ClientInfo second_client_;
   std::size_t first_position_;
   std::size_t second_position_;
-  std::size_t route_;
+  std::size_t first_route_;
+  std::size_t second_route_;
 };
 
 }  // namespace daa
