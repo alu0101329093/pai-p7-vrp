481b695e36e68264d865915e5a9356744b06db98
Jaime <alu0101329093@ull.edu.es>
2022-04-26 10:33:35 +0100

    Updated echange info

--- src/exchange_info.cpp
+++ src/exchange_info.cpp
@@ -5,11 +5,13 @@ namespace daa {
 ExchangeInfo::ExchangeInfo(const ClientInfo& first_client,
                            const ClientInfo& second_client,
                            std::size_t first_position,
-                           std::size_t second_position, std::size_t route)
+                           std::size_t second_position, std::size_t first_route,
+                           std::size_t second_route)
     : first_client_{first_client},
       second_client_{second_client},
       first_position_{first_position},
       second_position_{second_position},
-      route_{route} {}
+      first_route_{first_route},
+      second_route_{second_route} {}
 
 }  // namespace daa
