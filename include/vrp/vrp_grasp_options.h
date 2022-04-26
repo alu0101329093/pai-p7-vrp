c330599bd963c014159d5b18728bc67a7350b264
Jaime <alu0101329093@ull.edu.es>
2022-04-26 11:21:44 +0100

    Added local search

--- include/vrp/vrp_grasp_options.h
+++ include/vrp/vrp_grasp_options.h
@@ -2,7 +2,9 @@
 #define P7VRP_VRP_VRP_GRASP_OPTIONS_H_
 
 #include <cstdlib>
+#include <memory>
 
+#include "vrp/vrp_local_search.h"
 #include "vrp/vrp_options.h"
 
 namespace daa {
@@ -10,7 +12,8 @@ namespace daa {
 class VrpGraspOptions : public VrpOptions {
  public:
   VrpGraspOptions(std::size_t iterations, std::size_t random_solutions_amount,
-                  std::size_t max_unchanged_iterations);
+                  std::size_t max_unchanged_iterations,
+                  VrpLocalSearch* local_search);
   virtual ~VrpGraspOptions() override;
 
   inline std::size_t GetIterations() const { return iterations_; }
@@ -32,10 +35,18 @@ class VrpGraspOptions : public VrpOptions {
     max_unchanged_iterations_ = max_unchanged_iterations;
   }
 
+  inline const std::unique_ptr<VrpLocalSearch>& GetLocalSearch() {
+    return local_search_;
+  }
+  inline void SetLocalSearch(VrpLocalSearch* local_search) {
+    local_search_.reset(local_search);
+  }
+
  private:
   std::size_t iterations_;
   std::size_t random_solutions_amount_;
   std::size_t max_unchanged_iterations_;
+  std::unique_ptr<VrpLocalSearch> local_search_;
 };
 
 }  // namespace daa
--- src/vrp_grasp_options.cpp
+++ src/vrp_grasp_options.cpp
@@ -4,10 +4,12 @@ namespace daa {
 
 VrpGraspOptions::VrpGraspOptions(std::size_t iterations,
                                  std::size_t random_solutions_amount,
-                                 std::size_t max_unchanged_iterations)
+                                 std::size_t max_unchanged_iterations,
+                                 VrpLocalSearch* local_search)
     : iterations_{iterations},
       random_solutions_amount_{random_solutions_amount},
-      max_unchanged_iterations_{max_unchanged_iterations} {}
+      max_unchanged_iterations_{max_unchanged_iterations},
+      local_search_{local_search} {}
 
 VrpGraspOptions::~VrpGraspOptions() {}
 
